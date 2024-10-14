3dd4f743e959f18a97746a20018d56c08a1b26a6
shimaa-moftah13 <shimaamoftah152@gmail.com>
2024-10-15 01:46:16 +0300

    first commit

--- src/services/addressServices.ts
+++ src/services/addressServices.ts
@@ -1,21 +1,21 @@
-// import axios from 'axios';
-// const API_URL = 'http://localhost:5005/addresses';
+import axios from 'axios';
+const API_URL = 'http://localhost:5005/addresses';
 
-// export interface Address {
-//     userId?: number;
-//     name: string;
-//       street: string;
-//       city: string;
-//       zip: string;
+export interface Address {
+    userId?: number;
+    name: string;
+      street: string;
+      city: string;
+      zip: string;
 
-// }
+}
 
-// export const getAddresses = async () => {
-//   const response = await axios.get<Address[]>(API_URL);
-//   return response.data;
-// };
+export const getAddresses = async () => {
+  const response = await axios.get<Address[]>(API_URL);
+  return response.data;
+};
 
-// export const addAddress = async (address: Address) => {
-//   const response = await axios.post<Address>(API_URL, address);
-//   return response.data;
-// };
+export const addAddress = async (address: Address) => {
+  const response = await axios.post<Address>(API_URL, address);
+  return response.data;
+};
